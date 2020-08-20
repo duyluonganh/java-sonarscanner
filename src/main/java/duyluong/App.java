96d16980f879c04ddeb483265085bb78d9d8c0c0
Duy Luong <duyluong@kms-technology.com>
2020-08-20 16:44:18 +0700

    Add password

--- src/main/java/duyluong/App.java
+++ src/main/java/duyluong/App.java
@@ -4,8 +4,10 @@ package duyluong;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static final String PASSWORD = "A";
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World 123!" );
